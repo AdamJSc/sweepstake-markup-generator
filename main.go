26b3e43f2c02fdfcc65e3b16f0f7c85eab79d64e
Adam Scholey <hello@adamjs.net>
2022-09-27 07:48:47 +0100

    chore: wrap panic values with any type

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	req, err := http.NewRequest(http.MethodGet, "https://icanhazdadjoke.com", nil)
 	if err != nil {
-		panic(err)
+		panic(any(err))
 	}
 
 	req.Header.Set("User-Agent", "curl/7.64.1")
@@ -20,18 +20,18 @@ func main() {
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic(err)
+		panic(any(err))
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		panic(fmt.Sprintf("status code %d", resp.StatusCode))
+		panic(any(fmt.Sprintf("status code %d", resp.StatusCode)))
 	}
 
 	defer resp.Body.Close()
 
 	b, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		panic(any(err))
 	}
 
 	os.Mkdir("public", 0755)
@@ -40,11 +40,11 @@ func main() {
 
 	f, err := os.OpenFile(filename, os.O_WRONLY, os.ModeAppend)
 	if err != nil {
-		panic(err)
+		panic(any(err))
 	}
 
 	if _, err := fmt.Fprintf(f, "<p>icanhazdadjoke.com says...</p>\n<h1>%s</h1>\n", string(b)); err != nil {
-		panic(err)
+		panic(any(err))
 	}
 
 	log.Println("completed!")
